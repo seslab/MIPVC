537ab1bbf121637b2783c7d5aa4551c7f680f7de
beatle28c <nautilus28c@gmail.com>
2017-08-16 14:55:07 -0600

    actualizaciong

--- MIPVC/Analizador.py
+++ MIPVC/Analizador.py
@@ -27,7 +27,7 @@ def main():
 	refx2=12;
 	width_b=12;
 	heigh_b=2.5;
-	win = GraphWin("Análisis de Calidad de Energía",width=1000, height=600)
+	win = GraphWin("Análisis de Calidad de Energía",width=750, height=400)
 	win.setCoords(0,0,xgrid,ygrid)
 	BG = Image(Point(12,15), 'backg.gif')
 	BG.draw(win)
@@ -39,10 +39,13 @@ def main():
 	osc1=PQ.fft_osc();
 	
 	LastFile = Button(win, Point(refx,refy-2), width_b, heigh_b, "Última Lectura")
+	LastFile.label.setSize(10)
 	LastFile.activate()
 	SearchFile = Button(win, Point(refx,refy-6), width_b, heigh_b, "Buscar Archivo")
+	SearchFile.label.setSize(10)
 	SearchFile.activate()
 	Salir = Button(win, Point(refx,refy-10), width_b, heigh_b, "Salir")
+	Salir.label.setSize(10)
 	Salir.activate()
 	texto=["Amplitud: ","Frecuencia: ","TDH(%):  ","F.P: "];
 	meas=[0 for i in range(16)]
@@ -50,14 +53,14 @@ def main():
 		mensaje=Text(Point(refx-5,refy+44-(i*10)),"Medición"+str(i+1))
 		mensaje.setFace('arial')
 		mensaje.setStyle('bold')
-		mensaje.setSize(11)
+		mensaje.setSize(10)
 		mensaje.setTextColor("white")
 		mensaje.draw(win)
 		for u in range(0,4):
 			mensaje=Text(Point(refx-5,refy+42-(u*2)-(i*10)),texto[u])
 			mensaje.setFace('arial')
 			mensaje.setStyle('bold')
-			mensaje.setSize(11)
+			mensaje.setSize(10)
 			mensaje.setTextColor("black")
 			mensaje.draw(win)
 			
@@ -67,7 +70,7 @@ def main():
 			meas[u]=Text(Point(refx+5,refy+42-((u-4*i)*2)-(i*10)),"")
 			meas[u].setFace('arial')
 			meas[u].setStyle('bold')
-			meas[u].setSize(11)
+			meas[u].setSize(10)
 			meas[u].setTextColor("black")
 			meas[u].draw(win)
 	
